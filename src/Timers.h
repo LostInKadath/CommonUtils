68efc71a4933f3a777c483f2522e53c2e478e7e3
LostInKadath <forgotten.in.kadath@gmail.com>
2020-03-07 13:34:04 +0300

    Comments for time measuring functions

--- src/Timers.h
+++ src/Timers.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <string>
-#include <sstream>
 #include <chrono>
 
 namespace sra {
@@ -10,8 +9,15 @@ namespace time {
 class Timer
 {
 public:
-	template<typename Fn>
-	auto Measure(Fn&& fn)
+
+	/*Measures execution time. Gets lambda object which captures all needed parameters.
+	Usage:
+		ResultType Foo(int, bool, Arg const&);
+
+		auto result = timer.Measure([&] { return Foo(1, true, arg); });
+	*/
+	template <typename Lambda>
+	auto Measure(Lambda&& fn)
 	{
 		const auto start = std::chrono::steady_clock::now();
 		const auto result = fn();
@@ -21,6 +27,24 @@ public:
 		return result;
 	}
 
+	/*Measures execution time. Gets pointer to a function and its arguments.
+	Usage:
+		ResultType Foo(int, bool, Arg const&);
+
+		auto result = timer.Measure(Foo, 1, true, arg);
+	*/
+	template<typename FuncPointer, typename... Args>
+	auto Measure(FuncPointer fn, Args&&... args)
+	{
+		const auto start = std::chrono::steady_clock::now();
+		const auto result = fn(std::forward<Args>(args)...);
+		const auto stop = std::chrono::steady_clock::now();
+
+		m_duration = stop - start;
+		return result;
+	}
+
+public:
 	std::chrono::nanoseconds GetTimeNs() const { return m_duration; }
 	std::string GetTime() const;
 
